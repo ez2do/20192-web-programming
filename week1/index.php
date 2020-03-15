c8696e84c239ed9d363b52e1f46a2f7de57540a2
tuananh <tuananh2431998@gmail.com>
2020-03-15 17:45:46 +0700

    week1

--- week1/index.php
+++ week1/index.php
@@ -4,8 +4,8 @@
     <body> 
         <font size=5 color=”blue”>Welcome To My Page</font>
         <?php
-        print ("<br> Using PHP is not hard<br>");
+         print ("<br> Using PHP is not hard<br>");
         ?>
-        and you can learn to use it quickly! 
+        and you can learn to use it quickly! okok
     </body>
 </html>
